44d8deb7131f2f09cad5755b1de1f1355cdde73e
Dev4Future <65303904+AllanChristianG@users.noreply.github.com>
2020-12-03 11:34:44 -0300

    Added more Inserts

--- padoca.sql
+++ padoca.sql
@@ -20,5 +20,11 @@ CREATE TABLE tb_cliente (
 
 insert into tb_cliente(nome, cpf, celular, endereco, numero, cep) 
 	values ("Cliente", "33333333390", "099999999999", "Rua dos Esmeros", 8, "13133333");  
+insert into tb_cliente(nome, cpf, celular, endereco, numero, cep) 
+	values ("Lucas Gato", "33333333391", "099999999998", "Rua das Esmeraldas", 7, "13133334");
+insert into tb_cliente(nome, cpf, celular, endereco, numero, cep) 
+	values ("Bruno Santos", "33333333380", "099999999997", "Rua das Esmeraldinas", 6, "13133335");
+insert into tb_cliente(nome, cpf, celular, endereco, numero, cep) 
+	values ("Ana Cruz", "33333333370", "099999999996", "Rua das Esmeraldas negras", 5, "13133336");
 
 
